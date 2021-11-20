b2eec9e2bd15819b05636acda1b44e23452843ce
Mike Burgess <mddburgess@users.noreply.github.com>
2021-11-19 20:30:38 -0800

    Add request ID filter.

---
+++ backend/src/main/java/com/metricalsky/argent/backend/filter/RequestIdFilter.java
@@ -0,0 +1,31 @@
+package com.metricalsky.argent.backend.filter;
+
+import java.io.IOException;
+import java.util.UUID;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class RequestIdFilter extends OncePerRequestFilter {
+
+    private static final String X_REQUEST_ID = "X-Request-Id";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+        try {
+            var requestId = UUID.randomUUID().toString().substring(0, 8);
+            response.addHeader(X_REQUEST_ID, requestId);
+            MDC.put(X_REQUEST_ID, requestId);
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.remove(X_REQUEST_ID);
+        }
+    }
+}
