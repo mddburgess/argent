e71eecf4eac8e43d5fd8704b61c154e556b1a81c
Mike Burgess <mddburgess@users.noreply.github.com>
2021-12-03 22:05:17 -0800

    Add ledger entry header row.

--- frontend/src/components/ledger/LedgerEntryList.tsx
+++ frontend/src/components/ledger/LedgerEntryList.tsx
@@ -2,12 +2,14 @@ import api from "api";
 import ListGroup from "react-bootstrap/ListGroup";
 import LedgerEntryRow from "components/ledger/LedgerEntryRow";
 import AddLedgerEntryRow from "components/ledger/AddLedgerEntryRow";
+import LedgerEntryHeaderRow from "components/ledger/LedgerEntryHeaderRow";
 
 const LedgerEntryList = () => {
     const {data} = api.useListLedgerQuery();
     return (
         <ListGroup variant="flush">
-            <AddLedgerEntryRow />
+            <LedgerEntryHeaderRow/>
+            <AddLedgerEntryRow/>
             {data?.map(item => <LedgerEntryRow key={item.id} item={item} />)}
         </ListGroup>
     )
